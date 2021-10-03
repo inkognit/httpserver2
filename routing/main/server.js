d3f7293d46f5ad1aeaedd64feabf86a3ac3138ef
michael <mikhail.shypileu@betnbet.ru>
2021-10-03 22:09:07 +0300

    connect to html

--- routing/main/server.js
+++ routing/main/server.js
@@ -1,12 +1,25 @@
 const http = require("http");
+const fs = require("fs").promises;
 
 const port = 3330;
 const host = "localhost";
 
 const listeningServer = http.createServer((req, res) => {
+   fs.readFile("/home/michael/q/httpserver/routing/pages/mainPage.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
   res.writeHead(200);
-  res.end("my server");
+      // res.end("contents");
+            res.end(contents);
+  
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });
 });
-listeningServer.listen(port, host,() => {
-    console.log(`🚀🚀🚀 \nServer is running on http://${host}:${port}`);
+
+listeningServer.listen(port, host, () => {
+  console.log(`🚀🚀🚀 \n Server is running on http://${host}:${port}`);
 });
